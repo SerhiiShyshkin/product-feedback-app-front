ca615e5cd37d4bd378394fa768bd328cdd00816a
Serhii Shyshkin <s.b.shyshkin@gmail.com>
2022-08-31 16:59:33 +0200

    Added typography Jost to project and added typography to theme file.

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,4 +1,8 @@
 import { createTheme } from '@mui/material';
+import '@fontsource/jost/400.css';
+import '@fontsource/jost/500.css';
+import '@fontsource/jost/600.css';
+import '@fontsource/jost/700.css';
 
 declare module '@mui/material/styles' {
   interface BreakpointOverrides {
@@ -57,6 +61,67 @@ const theme = createTheme({
       mobile: 327,
       tablet: 689,
       desktop: 1110,
+    },
+  },
+
+  typography: {
+    fontFamily: 'Jost',
+    h1: {
+      fontSize: '1.5rem',
+      fontWeight: '700',
+      lineHeight: '2.188rem',
+      letterSpacing: '-0.021rem',
+    },
+
+    h2: {
+      fontSize: '1.25rem',
+      fontWeight: '700',
+      lineHeight: '1.813rem',
+      letterSpacing: '-0.016rem',
+    },
+
+    h3: {
+      fontSize: '1.125rem',
+      fontWeight: '700',
+      lineHeight: '1.625rem',
+      letterSpacing: '-0.016rem',
+    },
+
+    h4: {
+      fontSize: '0.875rem',
+      fontWeight: '700',
+      lineHeight: '1.25rem',
+      letterSpacing: '-0.013rem',
+    },
+
+    body1: {
+      fontSize: '1rem',
+      fontWeight: '400',
+      lineHeight: '1.438rem',
+    },
+
+    body2: {
+      fontSize: '0.938rem',
+      fontWeight: '400',
+      lineHeight: '1.375rem',
+    },
+
+    caption: {
+      fontSize: '0.813rem',
+      fontWeight: '600',
+      lineHeight: '1.188rem',
+    },
+
+    button: {
+      fontSize: '0.813rem',
+      fontWeight: '700',
+      lineHeight: '1.188rem',
+    },
+
+    overline: {
+      fontSize: '0.875rem',
+      fontWeight: '400',
+      lineHeight: '1.25rem',
     },
   },
 });
